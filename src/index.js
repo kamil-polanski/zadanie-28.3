679201680421880c2ad89e096b3403dd27ea9ec2
Kamil Polanski <polanski88@gmail.com>
2019-10-09 11:55:41 +0200

    Remove app files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store/store';
-import DevTools from './DevTools';
-import {
-	getCountries
-} from './actions/actions';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-render(
-  <Provider store={store}>
-    <div>
-      <h1>Inicjalizacja projektu</h1>
-      <DevTools />
-    </div>
+ReactDOM.render(<App />, document.getElementById('root'));
 
-  </Provider>,
-  document.getElementById('root')
-);
-
-store.dispatch(getCountries());
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
